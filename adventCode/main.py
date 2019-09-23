db2fcc244cee57d4d613defb21af4e9affde30a9
ElanaFelsi <elanaehrman@gmail.com>
2019-09-23 18:02:52 +0300

    part 2 complete

--- adventCode/main.py
+++ adventCode/main.py
@@ -1,6 +1,7 @@
 import os
 import overlaped_fabric
 
+
 def main():
     path = os.path.join(os.path.expanduser('~'),
                         'BootCamp',
@@ -8,8 +9,7 @@ def main():
                         'python-home-collections-ElanaFelsi',
                         'claims.txt')
 
-    overlaped_fabric.open_file(path)
-
+    overlaped_fabric.overlapped(path)
 
 
 if __name__ == '__main__':
--- adventCode/overlaped_fabric.py
+++ adventCode/overlaped_fabric.py
@@ -1,26 +1,38 @@
-def open_file(_path):
+def parameters(line):
+    param = line.split()
+    left, top = param[2].split(',')
+    top = top.replace(':', "")
+    wide, tall = param[3].split('x')
+    left, top, wide, tall = int(left), int(top), int(wide), int(tall)
+    id = param[0]
+    return left, top, wide, tall, id
+
+
+def overlapped(_path):
     d = {}
     with open(_path, 'r') as f:
         for line in f:
-            param = line.split()
-            left, top = param[2].split(',')
-            top = top.replace(':', "")
-            wide, tall = param[3].split('x')
-            left, top, wide, tall = int(left), int(top), int(wide), int(tall)
+            left, top, wide, tall, id = parameters(line)
             for x in range(wide):
                 for y in range(tall):
-                    if (left+x, top+y) in d.keys():
-                        d[(left+x, top+y)] += 1
+                    if (left + x, top + y) in d.keys():
+                        d[(left + x, top + y)] += 1
                     else:
-                        d[(left+x, top+y)] = 1
+                        d[(left + x, top + y)] = 1
+
     count = 0
     for val in d.values():
         if val >= 2:
             count += 1
     print(count)
 
-
-
-
-
-
+    with open(_path, 'r') as f:
+        for line in f:
+            left, top, wide, tall, id = parameters(line)
+            flag = True
+            for x in range(wide):
+                for y in range(tall):
+                    if d[(left + x, top + y)] > 1:
+                        flag = False
+            if flag:
+                print(id)
